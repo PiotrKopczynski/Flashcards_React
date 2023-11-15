6ddd496cb2d9dcccb6d8dbdb2d31b64489475dcf
Mishunkhan <89736636+Mishunkhan@users.noreply.github.com>
2023-11-15 10:28:50 +0100

    begynt med Deck

---
+++ Flashcards_React/ClientApp/src/components/BrowseDecks.js
@@ -0,0 +1,88 @@
+﻿import React, { useEffect, useState } from 'react';
+
+const DeckList = () => {
+    const [decks, setDecks] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Fetch decks from the server
+        fetch('deck/browsedecks') // Adjust the URL to match your API endpoint
+            .then(response => response.json())
+            .then(data => {
+                setDecks(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching decks:', error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Deck List</h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="deck-container">
+                    {decks.map(deck => (
+                        <div key={deck.deckId} className="card">
+                            <h2>{deck.title}</h2>
+                            <p>{deck.description}</p>
+                            {/* Render other properties as needed */}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DeckList;
+
+
+
+
+
+/*import React, { useEffect, useState } from 'react';
+
+const BrowseDecks = () => {
+    const [decks, setDecks] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Fetch decks from the server
+        fetch('deck/browsedecks/') // Adjust the URL to match your API endpoint
+            .then(response => response.json())
+            .then(data => {
+                setDecks(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching decks:', error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Deck List</h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="deck-container">
+                    {decks.map(deck => (
+                        <div key={deck.deckId} className="card">
+                            <h2>{deck.title}</h2>
+                            <p>{deck.description}</p>
+                            }
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default BrowseDecks;
+*/
---
+++ Flashcards_React/Controllers/DeckController.cs
@@ -0,0 +1,119 @@
+﻿/*using Flashcards_React.DAL;
+using Flashcards_React.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Flashcards_React.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DeckController : ControllerBase
+    {
+        private readonly IDeckRepository _deckRepository;
+        private readonly ILogger<DeckController> _logger;
+        private readonly UserManager<FlashcardsUser> _userManager;
+
+        public DeckController(IDeckRepository deckRepository, ILogger<DeckController> logger, UserManager<FlashcardsUser> userManager)
+        {
+            _deckRepository = deckRepository;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        private static readonly IEnumerable<Deck> Decks = new[]
+        {
+            new Deck
+            {
+                Title = "Norwegian flashcards",
+                Description = "This is a demo deck: A deck containing basic Norwegian language cards for learning.",
+                FlashcardsUserId = "demo"
+            },
+            new Deck
+            {
+                Title = "Science quiz flashcard",
+                Description = "This is a demo deck: A deck containing science questions and answers ",
+                FlashcardsUserId = "demo"
+            }
+        };
+
+        [HttpGet]
+        [Route("browsedecks")]
+        public ActionResult<IEnumerable<Deck>> BrowseDecks()
+        {
+            return Ok(Decks);
+        }
+    }
+}*/
+
+
+
+
+
+using Flashcards_React.DAL;
+using Flashcards_React.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Flashcards_React.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeckController : ControllerBase
+    {
+        private readonly IDeckRepository _deckRepository;
+        private readonly ILogger<DeckController> _logger;
+        private readonly UserManager<FlashcardsUser> _userManager;
+
+        public DeckController(IDeckRepository deckRepository, ILogger<DeckController> logger, UserManager<FlashcardsUser> userManager)
+        {
+            _deckRepository = deckRepository;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("browsedecks")]
+        public async Task<ActionResult<IEnumerable<Deck>>> BrowseDecks()
+        {
+            String searchString = "";
+            
+            try
+            {
+                var flashcardsUserId = _userManager.GetUserId(this.User) ?? "";
+                IEnumerable<Deck> decks;
+
+                if (string.IsNullOrEmpty(searchString))
+                {
+                    decks = await _deckRepository.GetAll(flashcardsUserId);
+                }
+                else
+                {
+                    decks = await _deckRepository.SearchDecksByTitle(flashcardsUserId, searchString);
+                }
+
+                if (decks == null)
+                {
+                    _logger.LogError("[DeckController] Deck list not found.");
+                    return NotFound("Deck list not found.");
+                }
+
+                
+                return Ok(decks);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "[DeckController] Error while fetching decks.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error");
+            }
+        }
+    }
+}
