77cc702adf82987b8dac9b51860df3275362fcde
Mishunkhan <89736636+Mishunkhan@users.noreply.github.com>
2023-11-15 13:22:38 +0100

    updated code with Browse.js and controller

--- Flashcards_React/ClientApp/src/components/BrowseDecks.js
+++ Flashcards_React/ClientApp/src/components/BrowseDecks.js
@@ -1,58 +1,13 @@
 ﻿import React, { useEffect, useState } from 'react';
 
-const DeckList = () => {
-    const [decks, setDecks] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        // Fetch decks from the server
-        fetch('deck/browsedecks') // Adjust the URL to match your API endpoint
-            .then(response => response.json())
-            .then(data => {
-                setDecks(data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error fetching decks:', error);
-                setLoading(false);
-            });
-    }, []);
-
-    return (
-        <div>
-            <h1>Deck List</h1>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <div className="deck-container">
-                    {decks.map(deck => (
-                        <div key={deck.deckId} className="card">
-                            <h2>{deck.title}</h2>
-                            <p>{deck.description}</p>
-                            {/* Render other properties as needed */}
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    );
-};
-
-export default DeckList;
-
-
-
-
-
-/*import React, { useEffect, useState } from 'react';
-
 const BrowseDecks = () => {
     const [decks, setDecks] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // Fetch decks from the server
-        fetch('deck/browsedecks/') // Adjust the URL to match your API endpoint
+        const searchString = 'your_search_string'; // Replace 'your_search_string' with the actual search string
+        fetch(`api/deck/browsedecks?searchString=${searchString}`)
             .then(response => response.json())
             .then(data => {
                 setDecks(data);
@@ -75,7 +30,6 @@ const BrowseDecks = () => {
                         <div key={deck.deckId} className="card">
                             <h2>{deck.title}</h2>
                             <p>{deck.description}</p>
-                            }
                         </div>
                     ))}
                 </div>
@@ -85,4 +39,3 @@ const BrowseDecks = () => {
 };
 
 export default BrowseDecks;
-*/
--- Flashcards_React/Controllers/DeckController.cs
+++ Flashcards_React/Controllers/DeckController.cs
@@ -1,74 +1,15 @@
-﻿/*using Flashcards_React.DAL;
+﻿using Flashcards_React.DAL;
 using Flashcards_React.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-
-namespace Flashcards_React.Controllers
-{
-    [Route("[controller]")]
-    [ApiController]
-    public class DeckController : ControllerBase
-    {
-        private readonly IDeckRepository _deckRepository;
-        private readonly ILogger<DeckController> _logger;
-        private readonly UserManager<FlashcardsUser> _userManager;
-
-        public DeckController(IDeckRepository deckRepository, ILogger<DeckController> logger, UserManager<FlashcardsUser> userManager)
-        {
-            _deckRepository = deckRepository;
-            _logger = logger;
-            _userManager = userManager;
-        }
-
-        private static readonly IEnumerable<Deck> Decks = new[]
-        {
-            new Deck
-            {
-                Title = "Norwegian flashcards",
-                Description = "This is a demo deck: A deck containing basic Norwegian language cards for learning.",
-                FlashcardsUserId = "demo"
-            },
-            new Deck
-            {
-                Title = "Science quiz flashcard",
-                Description = "This is a demo deck: A deck containing science questions and answers ",
-                FlashcardsUserId = "demo"
-            }
-        };
-
-        [HttpGet]
-        [Route("browsedecks")]
-        public ActionResult<IEnumerable<Deck>> BrowseDecks()
-        {
-            return Ok(Decks);
-        }
-    }
-}*/
-
-
-
-
-
-using Flashcards_React.DAL;
-using Flashcards_React.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Flashcards_React.Controllers
 {
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class DeckController : ControllerBase
     {
         private readonly IDeckRepository _deckRepository;
@@ -82,8 +23,8 @@ namespace Flashcards_React.Controllers
             _userManager = userManager;
         }
 
-        [HttpGet]
-        [Route("BrowseDecks")]
+        [HttpGet("searchString:string")]
+        [Route("browsedecks")]
         public async Task<IEnumerable<Deck>> BrowseDecks(string searchString) // Double check that you can pass parameters like this through link.
         // The BrowseDecks View function allows the use to browse through the existing decks, create new decks and
         // search for specific decks using searchString. In addition, pagination functonality is implemented using the PaginatedList<> class.
@@ -103,17 +44,11 @@ namespace Flashcards_React.Controllers
             else
             {
                 decks = await _deckRepository.SearchDecksByTitle(flashcardsUserId, searchString);
-                }
-
                 if (decks == null)
                 {
                     _logger.LogError("[DeckController] Deck list not found while executing _deckRepository.SearchDecksByTitle()");
                     return new List<Deck>(); ;
                 }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "[DeckController] Error while fetching decks.");
-                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error");
             }
 
             return decks;
