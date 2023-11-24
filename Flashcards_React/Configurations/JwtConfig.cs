9ea9e9ab9619a31ca840e5c14c98c233b3c18e33
Mishunkhan <89736636+Mishunkhan@users.noreply.github.com>
2023-11-24 17:15:21 +0100

    deleted everything

--- Flashcards_React/Configurations/JwtConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Flashcards_React.Configurations
-{
-    public class JwtConfig
-    {
-        public string Secret { get; set; } = string.Empty;
-        public TimeSpan ExpiryTimeFrame { get; set; }
-    }
-}
