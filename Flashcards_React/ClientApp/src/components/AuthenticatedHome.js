67231079f51e5e75d0ff84779181ee7148822ac8
Zarmina Desktop <zarminaehsan@icloud.com>
2023-11-26 18:00:17 +0100

    incomplete signed in user landing page

--- Flashcards_React/ClientApp/src/components/AuthenticatedHome.js
+++ Flashcards_React/ClientApp/src/components/AuthenticatedHome.js
@@ -1,11 +1,23 @@
-  import React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SleepyCatAnimation from './SleepyCatAnimation';
+import BulbAnimation from './BulbAnimation';
+import './AuthenticatedHome.css';
+
   
-  const AuthenticatedHome = () => {
+const AuthenticatedHome = () => {
     return (
         <div>
-            <p>Authenticatedafafas</p>
-        </div>
-    );
-  };
+            <h1 className=" text-center mb-0 fs-1 heading">Ready to practice?</h1>
+            <div className="cat-wakeup">
+                <BulbAnimation></BulbAnimation>
+                <SleepyCatAnimation></SleepyCatAnimation>
+            </div>
+            <div className="practice-btn">
+                <Link to="/BrowseDecks" id="sleepy-cat-btn" className="btn btn-primary me-3">Let's go!</Link>
+            </div>
+        </div >
+);
+};
   
   export default AuthenticatedHome;
---
+++ Flashcards_React/ClientApp/src/components/SleepyCatAnimation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './SleepyCatAnimation.css'
+
+const SleepyCatAnimation = () => {
+    return (
+        <div class="cat-container">
+            <div class="cat-shadow"></div>
+            <div class="cat">
+                <div class="ear"></div>
+                <div class="eye"></div>
+                <div class="mouth"></div>
+                <div class="nose"></div>
+                <div class="tail"></div>
+                <div class="cat-body"></div>
+                <div class="cat-bubble"></div>
+            </div>
+        </div>
+    );
+};
+
+export default SleepyCatAnimation;
