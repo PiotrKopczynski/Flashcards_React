947982bd053b2e6a256d24e2672b7317ca97c812
ToriRays <98709974+ToriRays@users.noreply.github.com>
2023-11-26 14:17:42 +0100

    added update and delete functions which in fact do not delete

--- Flashcards_React/ClientApp/src/components/BrowseFlashcards.js
+++ Flashcards_React/ClientApp/src/components/BrowseFlashcards.js
@@ -1,7 +1,7 @@
 ﻿import React, { useEffect, useState, useContext } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import api from '../api/axios';
-import './StyleFile.css'; 
+import './StyleFile.css';
 
 const BrowseFlashcards = () => {
     const location = useLocation();
@@ -28,41 +28,62 @@ const BrowseFlashcards = () => {
         getFlashcards();
     }, []);
 
-
     const handleCreateFlashcardButton = (deck) => {
         navigate(`/createflashcard/${deck.deckId}`, { state: { deck } });
-    }
+    };
+
+    const handleDeleteFlashcardButton = (flashcardId) => {
+        navigate(`/deleteflashcard/${flashcardId}`, { state: { flashcardId } });
+    };
+
+    const handleUpdateFlashcardButton = (flashcardId, deckId) => {
+        navigate(`/updateflashcard/${flashcardId}`, { state: { flashcardId, deckId } });
+    };
+
+
+
     const handleBackToDeckButton = () => {
         navigate(`/browsedecks`);
-    }
+    };
 
     return (
         <div>
             <h1>Flashcards</h1>
-            {loading?(
-                <p> Loading...</p >
+            {loading ? (
+                <p>Loading...</p>
             ) : (
-
                 <>
-                     <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="row row-cols-1 row-cols-md-2 g-4">
                         {flashcards.map((flashcard) => (
-                             <div key={flashcard.FlashcardId} className="col">
+                            <div key={flashcard.FlashcardId} className="col">
                                 <div className="card text-center" style={{ width: '18rem' }}>
-                                  <div className="card-body">
-                                    <p className="card-text">Question: {flashcard.question}</p>
-                                    <p className="card-text">Answer: {flashcard.answer}</p>
-                                    <p className="card-text">Notes: {flashcard.notes}</p>
-                                  </div>
+                                    <div className="card-body">
+                                        <p className="card-text">Question: {flashcard.question}</p>
+                                        <p className="card-text">Answer: {flashcard.answer}</p>
+                                        <p className="card-text">Notes: {flashcard.notes}</p>
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() => handleDeleteFlashcardButton(flashcard.FlashcardId)}>
+                                            Delete
+                                        </button>
+                                        <button
+                                            className="btn btn-primary mx-2"
+                                            onClick={() => handleUpdateFlashcardButton(flashcard.FlashcardId, deck.deckId)}>
+                                            Update
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         ))}
-                     </div>
-                        <button class="btn btn-primary mx-5 mt-2 mb-5" onClick={() => handleCreateFlashcardButton(deck)}>
-                            Create a Flashcard
-                        </button>
-                        <button class="btn btn-primary mx-5 mt-2 mb-5" onClick={() => handleBackToDeckButton()}>
-                            Back to Decks
-                        </button>
+                    </div>
+                    <button
+                        className="btn btn-primary mx-5 mt-2 mb-5"
+                        onClick={() => handleCreateFlashcardButton(deck)}>
+                        Create a Flashcard
+                    </button>
+                    <button className="btn btn-primary mx-5 mt-2 mb-5" onClick={handleBackToDeckButton}>
+                        Back to Decks
+                    </button>
                 </>
             )}
         </div>
