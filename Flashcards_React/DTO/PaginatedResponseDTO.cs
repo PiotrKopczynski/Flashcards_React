0bcd4e8764aacbf6b677f07d68f75a1c8f8e58e7
Mishunkhan <89736636+Mishunkhan@users.noreply.github.com>
2023-11-27 20:21:39 +0100

    fixed issues with my master merge

---
+++ Flashcards_React/DTO/PaginatedResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using Flashcards_React.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flashcards_React.DTO
+{
+    public class PaginatedResponseDTO<T>
+    // This class is utilized such that the unit tests are easier to write
+    {
+        [Required]
+        public required PaginatedList<T> List { get; set; }
+        [Required]
+        public required int TotalPages { get; set; }
+        [Required]
+        public required bool HasPreviousPage { get; set; }
+        [Required]
+        public required bool HasNextPage { get; set; }
+    }
+}
