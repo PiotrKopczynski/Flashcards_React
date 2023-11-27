bf8d807b85c9c05817f12969fb7d595672f430f7
Zarmina Desktop <zarminaehsan@icloud.com>
2023-11-27 18:09:44 +0100

    complete login form design including responsivness

--- Flashcards_React/ClientApp/src/components/authentication/Login.js
+++ Flashcards_React/ClientApp/src/components/authentication/Login.js
@@ -67,69 +67,52 @@ const Login = () => {
 
     return (
         <div className="login-form-section">
-            <div className="justify-content-center">
+            <div className="row">
                 <div className="col-xl-5 col-lg-6 col-md-7 col-sm-8 col-10 mt-2">
                     <div className="card login-card mx-3">
-                        
                         <div className="card-body">
-
-
                             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
                             <h1 className="fs-2 text-center">Welcome back!</h1>
-                            <hr />
-
-                            <div className="row justify-content-center">
-                                <div className=" col-xl-8 col-lg-9 col-md-10 col-sm-10 col-10 my-3">
-
-                                    
-
-                                    <form onSubmit={handleSubmit}> 
-                                        <div className="form-floating mb-3">
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                placeholder="Email"
-                                                id="email"
-                                                ref={emailRef}
-                                                autoComplete="on"
-                                                onChange={(e) => setEmail(e.target.value)}
-                                                value={email}
-                                                required
-                                            /> 
-                                            <label htmlFor="email">Email</label>
-                                        </div>
-                                        <div className="form-floating mb-3">
-                                            
-                                            <input
-                                                type="password"
-                                                className="form-control"
-                                                placeholder="Password"
-                                                id="password"
-                                                onChange={(e) => setPwd(e.target.value)}
-                                                value={pwd}
-                                                required
-                                            />
-                                            <label htmlFor="password">Password</label>
-                                        </div>
-                                        <div>
-                                            <button className="w-100 btn btn-lg btn-primary mb-3">Sign In</button>
-                                        </div>
-
-                                        <div>
-                                            <p className="fs-5 text-center"> Need an Account?</p>
-                                             <span className="line">
-                                                    <Link to="/Register" id="form-text">Sign Up</Link>
-                                             </span>
-                                        </div>
-                                    </form>
-                                    
+                            <hr className="custom-hr" />
+                            <form onSubmit={handleSubmit}> 
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Email"
+                                        id="email"
+                                        ref={emailRef}
+                                        autoComplete="on"
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        value={email}
+                                        required
+                                    /> 
+                                    <label htmlFor="email">Email</label>
                                 </div>
-                            </div>
+                                <div className="form-floating mb-3">
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            placeholder="Password"
+                                            id="password"
+                                            onChange={(e) => setPwd(e.target.value)}
+                                            value={pwd}
+                                            required
+                                        />
+                                        <label htmlFor="password">Password</label>
+                                </div>
+                                <div className="text-center">
+                                    <button className="w-100 btn btn-lg btn-primary mb-3">Sign In</button>
+                                    <p className="fs-5"> Need an Account?</p>
+                                    <span><Link to="/Register">Sign Up</Link></span>
+                                </div>
+                            </form>     
                         </div>
                     </div>
                 </div>
             </div>
-         </div>
+        </div>
+         
     );
 };
 
