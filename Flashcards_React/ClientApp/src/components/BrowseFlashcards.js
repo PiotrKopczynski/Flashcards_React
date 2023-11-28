45a8e41f69530b34e3713b6f9b6d89ebdc4dda45
Mishunkhan <89736636+Mishunkhan@users.noreply.github.com>
2023-11-29 00:10:41 +0100

    added details view to flashcard

--- Flashcards_React/ClientApp/src/components/BrowseFlashcards.js
+++ Flashcards_React/ClientApp/src/components/BrowseFlashcards.js
@@ -58,16 +58,10 @@ const BrowseFlashcards = () => {
     const handleCreateFlashcardButton = (deck) => {
         navigate(`/createflashcard/${deck.deckId}`, { state: { deck } });
     };
-
-    const handleDeleteFlashcardButton = (flashcard,deck) => {
-        navigate(`/deleteflashcard/${flashcard.flashcardId}`, { state: { flashcard,deck} });
+   
+    const handleDetailButton = (flashcard, deck) => {
+        navigate(`/detailflashcard/${flashcard.flashcardId}`, { state: { flashcard, deck } });
     };
-
-    const handleUpdateFlashcardButton = (flashcard,deck) => {
-        navigate(`/updateflashcard/${flashcard.flashcardId}`, { state: { flashcard,deck} });
-    };
-
-
 
     const handleBackToDeckButton = () => {
         navigate(`/browsedecks`);
@@ -99,14 +93,9 @@ const BrowseFlashcards = () => {
                                                 <FontAwesomeIcon icon={showContent ? faEyeSlash : faEye} aria-hidden="true" />Show answer
                                             </button>
                                             <button
-                                                className="btn btn-danger"
-                                                onClick={() => handleDeleteFlashcardButton(flashcard,deck)}>
-                                                Delete
-                                            </button>
-                                            <button
                                                 className="btn btn-primary mx-2"
-                                                onClick={() => handleUpdateFlashcardButton(flashcard,deck)}>
-                                                Update
+                                                onClick={() => handleDetailButton(flashcard, deck)}>
+                                                Inspect
                                             </button>
                                     </div>
                                 </div>
