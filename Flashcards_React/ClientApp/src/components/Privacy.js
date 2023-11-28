34023c41a77fa8a54094c41bbc29ae05d36525d7
Zarmina Desktop <zarminaehsan@icloud.com>
2023-11-28 16:02:26 +0100

    added privacy component

---
+++ Flashcards_React/ClientApp/src/components/Privacy.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+
+const Privacy = () => {
+    return (
+        <section class="section-intro">
+            <h1>We will update our privacy policy soon</h1>
+        </section>
+    );
+};
+
+export default Privacy;
